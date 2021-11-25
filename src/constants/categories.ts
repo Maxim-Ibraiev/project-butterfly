d800fff1c6bfee0b70ec3234d1bd238b3b0f9112
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2021-11-25 21:25:19 +0200

    fixed cash mode

--- src/constants/categories.ts
+++
@@ -1,1 +0,0 @@
-export default ['dress', 'suit', 'jeans', 'largeSizes', 'shirts']
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,6 +8,3 @@ export const mobileUpper = 768
 export const tableLesser = 1023
 export const tableUpper = 1024
 export const desktop = 1200
-
-export { default as CATEGORIES } from './CATEGORIES'
-export { default as PRODUCTS } from './PRODUCTS'
